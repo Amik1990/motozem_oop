f0e19fb95c7657f87150f4015d037c0ba674c5cb
Amik1990 <djarnot@seznam.cz>
2025-06-01 13:00:52 +0200

    added tests to test_header_page.py.

---
+++ pages/header_page.py
@@ -0,0 +1,60 @@
+from pages.base_page import BasePage
+from playwright.sync_api import expect
+
+class HeaderPage(BasePage):
+
+    def load(self):
+        self.page.goto("https://www.motozem.cz/")
+        button = self.page.get_by_role("link", name="OK", exact=True)
+        self.click(button)
+
+    def informace_menu_is_visible_when_hover_over(self):
+        informace_button = self.page.get_by_role("link", name="Informace")
+        informace_menu = self.page.get_by_text("O nás Obchodní podmínky Ochrana osobních údajů Velkoobchod Prodejny Články Vnit")
+        informace_button.hover()
+        expect(informace_menu).to_be_visible()
+
+
+    def informace_menu_content(self):
+        informace_button = self.page.get_by_role("link", name="Informace")
+        informace_button.hover()
+
+        informace_items = [
+            {"selector": self.page.get_by_title("O nás"), "text": "O nás"},
+            {"selector": self.page.get_by_title("Obchodní podmínky"), "text": "Obchodní podmínky"},
+            {"selector": self.page.get_by_title("Ochrana osobních údajů"), "text": "Ochrana osobních údajů"},
+            {"selector": self.page.get_by_title("Velkoobchod"), "text": "Velkoobchod"},
+            {"selector": self.page.get_by_title("Prodejny"), "text": "Prodejny"},
+            {"selector": self.page.get_by_title("Vnitřní oznamovací systém"), "text": "Vnitřní oznamovací systém"},
+            {"selector": self.page.get_by_title("Výměna, vrácení, reklamace"), "text": "Výměna, vrácení, reklamace"},
+            {"selector": self.page.get_by_title("Kariéra"), "text": "Kariéra"},
+        ]
+
+        for item in informace_items:
+            menu_item = item["selector"]
+            expect(menu_item).to_be_visible()
+            expect(menu_item).to_have_text(item["text"])
+
+    def prodejny_menu_is_visible_when_hover_over(self):
+        prodejny_button = self.page.get_by_role("banner").get_by_role("link", name="Prodejny")
+        prodejny_menu = self.page.locator("li").filter(has_text="Prodejny Dobrá Brno Čestlice").get_by_role("list")
+        prodejny_button.hover()
+        expect(prodejny_menu).to_be_visible()
+
+    def prodejny_menu_content(self):
+        prodejny_button = self.page.get_by_role("banner").get_by_role("link", name="Prodejny")
+        prodejny_button.hover()
+
+        prodejny_items = [
+            {"selector": self.page.get_by_title("Dobrá"), "text": "Dobrá"},
+            {"selector": self.page.get_by_title("Brno"), "text": "Brno"},
+            {"selector": self.page.get_by_title("Čestlice"), "text": "Čestlice"},
+            {"selector": self.page.get_by_title("Senec"), "text": "Senec"},
+            {"selector": self.page.get_by_title("Plzeň"), "text": "Plzeň"},
+            {"selector": self.page.get_by_title("Košice"), "text": "Košice"},
+        ]
+
+        for item in prodejny_items:
+            menu_item = item["selector"]
+            expect(menu_item).to_be_visible()
+            expect(menu_item).to_have_text(item["text"])
---
+++ tests/test_header_page.py
@@ -0,0 +1,16 @@
+from pages.header_page import HeaderPage
+from pages.home_page import HomePage
+from playwright.sync_api import expect, Page
+
+def test_informace_is_present(load_header_page):
+    load_header_page.informace_menu_is_visible_when_hover_over()
+
+def test_informace_menu_content_is_present(load_header_page):
+    load_header_page.informace_menu_content()
+
+
+def test_prodejny_menu_is_visible(load_header_page):
+    load_header_page.prodejny_menu_is_visible_when_hover_over()
+
+def test_prodejny_menu_content_is_visible(load_header_page):
+    load_header_page.prodejny_menu_content()
