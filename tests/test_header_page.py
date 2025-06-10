9e51c929a6e22aa9ca98730d8382a9c45bd815b2
Amik1990 <djarnot@seznam.cz>
2025-06-10 12:43:03 +0200

    deleted import rows

--- tests/test_header_page.py
+++ tests/test_header_page.py
@@ -1,6 +1,3 @@
-from pages.header_page import HeaderPage
-from pages.home_page import HomePage
-from playwright.sync_api import expect, Page
 
 def test_informace_is_present(load_header_page):
     load_header_page.informace_menu_is_visible_when_hover_over()
--- tests/test_motozem_dobra_page.py
+++ tests/test_motozem_dobra_page.py
@@ -1,6 +1,4 @@
-from pages.header_page import HeaderPage
-from pages.home_page import HomePage
-from playwright.sync_api import expect, Page
+
 
 def test_lokace_dobra(load_motozem_dobra):
     load_motozem_dobra.lokace_dobra_google_maps_open()
