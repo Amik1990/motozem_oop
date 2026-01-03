aa68d6265640a903a12f47370f6e1c10e5b6679f
Amik1990 <djarnot@seznam.cz>
2026-01-03 11:21:55 +0100

    novy komentar

--- report.bat
+++ report.bat
@@ -1,4 +1,14 @@
 @echo off
+REM --- CO JE TENTO SOUBOR? ---
+REM Toto je "zkratka" pro Windows.
+REM Místo abyste museli otevírat terminál a psát příkaz "allure serve allure-results",
+REM stačí na tento soubor dvakrát kliknout myší.
+REM
+REM Co to udělá:
+REM 1. Vezme výsledky testů ze složky 'allure-results'.
+REM 2. Vygeneruje z nich hezkou webovou stránku (report).
+REM 3. Automaticky ji otevře ve vašem prohlížeči.
+
 echo Generuji Allure report...
 allure serve allure-results
 pause
